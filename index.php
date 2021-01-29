b940fe5d33834e8a89c94bece76b27c38e973696
Jason Velarde <gotnovalues@gmail.com>
2021-01-28 22:38:31 -0500

    fixed target

--- index.php
+++ index.php
@@ -14,25 +14,25 @@
 
 <section id="welcome-section">
   <h1>Jay's Portfolio</h1>
-  <p class="workSans">full-stack web developer</p>
+  <p class="workSans">full-stack Web developer</p>
 </section>
 <section class="projects">
 <section id="projects">
   <h2>My Projects</h2>
   <div id="projects-grid">
-    <a href="https://codepen.io/got0values/pen/WNGJwyV" targe="_blank" class="project-tile">
+    <a href="https://codepen.io/got0values/pen/WNGJwyV" targe=t"_blank" class="project-tile">
       <img src="http://placekitten.com/350/300" target="_blank" class="product-image">
       <p>Landing Page</p>
     </a>
-    <a href="https://codepen.io/got0values/pen/poEVNJm" targe="_blank" class="project-tile">
+    <a href="https://codepen.io/got0values/pen/poEVNJm" targe=t"_blank" class="project-tile">
       <img src="http://placekitten.com/350/300" target="_blank" class="product-image">
       <p>Tech Doc</p>
     </a>
-    <a href="https://codepen.io/got0values/pen/QWKmxYj" targe="_blank" class="project-tile">
+    <a href="https://codepen.io/got0values/pen/QWKmxYj" targe=t"_blank" class="project-tile">
       <img src="http://placekitten.com/350/300" target="_blank" class="product-image">
       <p>Survey</p>
     </a>
-    <a href="https://codepen.io/got0values/pen/QWKmprJ" targe="_blank" class="project-tile">
+    <a href="https://codepen.io/got0values/pen/QWKmprJ" target="_blank" class="project-tile">
       <img src="http://placekitten.com/350/300" target="_blank" class="product-image">
       <p>Tribute Page</p>
     </a>
