f1693b952cf4e05ccf62c3cb8eacc54692aaf2a0
got0values <gotnovalues@gmail.com>
2021-01-29 00:50:44 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+<div id="container">
+
+<nav id="navbar">
+  <ul>
+    <li><a href="#projects" class="nav-list">Projects</a></li>
+    <li><a href="#contact-section" class="nav-list">Contact</a></li>
+  </ul>
+</nav>
+
+<section id="welcome-section">
+  <h1>Jay's Portfolio</h1>
+  <p class="workSans">full-stack web developer</p>
+</section>
+<section class="projects">
+<section id="projects">
+  <h2>My Projects</h2>
+  <div id="projects-grid">
+    <a href="https://codepen.io/got0values/pen/WNGJwyV" target="_blank" class="project-tile">
+      <img src="http://placekitten.com/350/300" target="_blank" class="product-image">
+      <p>Landing Page</p>
+    </a>
+    <a href="https://codepen.io/got0values/pen/poEVNJm" target="_blank" class="project-tile">
+      <img src="http://placekitten.com/350/300" target="_blank" class="product-image">
+      <p>Tech Doc</p>
+    </a>
+    <a href="https://codepen.io/got0values/pen/QWKmxYj" target="_blank" class="project-tile">
+      <img src="http://placekitten.com/350/300" target="_blank" class="product-image">
+      <p>Survey</p>
+    </a>
+    <a href="https://codepen.io/got0values/pen/QWKmprJ" target="_blank" class="project-tile">
+      <img src="http://placekitten.com/350/300" target="_blank" class="product-image">
+      <p>Tribute Page</p>
+    </a>
+  </div>
+</section>
+</section>
+  
+<section id="contact-section">
+  <h2>Contact Me</h2>
+  <a href="https://github.com/got0values" id="profile-link" target="_blank">My GitHub</a>
+</section>
+  
+</div>
+</html>
