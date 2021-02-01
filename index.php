48e8b008bc4ec023c008c8dce3951e20663c2aff
got0values <gotnovalues@gmail.com>
2021-02-01 07:18:59 +0000

    new fading and style

--- index.php
+++ index.php
@@ -24,29 +24,49 @@
 <section id="projects">
   <h2>My Projects</h2>
   <div id="projects-grid">
-    <a href="https://jayvel.com" target="_blank" class="project-tile">
-      <img src="images/jayvel.png" target="_blank" class="product-image">
-      <p>JayVel Photo</p>
-    </a>
-    <a href="https://jvreads.com" target="_blank" class="project-tile">
-      <img src="/images/jvreads.png" target="_blank" class="product-image">
-      <p>JVReads</p>
-    </a>
-    <a href="https://stemcadia.com" target="_blank" class="project-tile">
-      <img src="images/stemcadia.png" target="_blank" class="product-image">
-      <p>STEMcadia</p>
-    </a>
-    <a href="https://childrens.freeportlibrary.info" target="_blank" class="project-tile">
-      <img src="images/fmlcr.png" target="_blank" class="product-image">
-      <p style="font-size: 14px;">Freeport Memorial Library<br>Children's Room</p>
-    </a>
+    <div class="imgContainer">
+        <a href="https://longislandprofessionalheadshots.com" target="_blank" class="project-tile">
+            <img src="images/lbtq.png" target="_blank" class="product-image">
+            <p>LibraryThing Quiz</p>
+        </a>
+     <div class="cntrTxt">PHP<br>+<br>JavaScript<br>+<br>MySQL</div>
+    </div>
+    <div class="imgContainer">
+        <a href="https://jvreads.com" target="_blank" class="project-tile">
+            <img src="/images/jvreads.png" target="_blank" class="product-image">
+            <p>JVReads</p>
+        </a>
+        <div class="cntrTxt">Python<br>+<br>JavaScript<br>+<br>SQLite</div>
+    </div>
+    <div class="imgContainer">
+        <a href="https://childrens.freeportlibrary.info" target="_blank" class="project-tile">
+            <img src="images/fmlcr.png" target="_blank" class="product-image">
+            <p style="font-size: 14px;">Freeport Memorial Library<br>Children's Room</p>
+        </a>
+        <div class="cntrTxt">WordPress<br>+<br>JavaScript<br>+<br>PHP</div>
+    </div>
+    <div class="imgContainer">
+        <a href="https://stemcadia.com" target="_blank" class="project-tile">
+            <img src="images/stemcadia.png" target="_blank" class="product-image">
+            <p>STEMcadia</p>
+        </a>
+        <div class="cntrTxt">WordPress<br>+<br>JavaScript<br>+<br>PHP</div>
+    </div>
+    <div class="imgContainer">
+        <a href="https://jayvel.com" target="_blank" class="project-tile">
+            <img src="images/jayvel.png" target="_blank" class="product-image">
+            <p>JayVel Photo</p>
+        </a>
+        <div class="cntrTxt">WordPress<br>+<br>JavaScript</div>
+    </div>
+    
   </div>
 </section>
 </section>
   
 <section id="contact-section">
-  <h2>Contact Me</h2>
-  <a href="https://github.com/got0values" id="profile-link" target="_blank">My GitHub</a>
+  <p>jayvelphoto@gmail.com</p>
+  <a href="https://github.com/got0values" id="profile-link" target="_blank">GitHub</a>
 </section>
   
 </div>
