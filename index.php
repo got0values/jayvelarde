80f6df1e76a393c0d400cc5d3665d9ff77991ca9
got0values <gotnovalues@gmail.com>
2021-04-09 07:05:22 +0000

    new style

--- index.php
+++ index.php
@@ -1,73 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <link rel="stylesheet" href="style.css">
-    <title>Jason Velarde</title>
-  </head>
-<div id="container">
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">  
+        <link rel="stylesheet" href="style.css">
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+        <title>Jason Velarde</title>
+    </head>
 
-<nav id="navbar">
-  <div id="jv">
-    Jason Velarde
-  </div>
-  <ul>
-    <li><a href="#projects" class="nav-list">Projects</a></li>
-    <li><a href="#contact-section" class="nav-list">Contact</a></li>
-  </ul>
-</nav>
+<body>
+    <nav id="navbar">
+        <div id="jv" class="dancingscript">
+            Jason Velarde
+        </div>
+        <ul>
+            <li><a href="#projects" class="nav-list">Projects</a></li>
+            <li><a href="#me" class="nav-list">About Me</a></li>
+            <li><a href="#contact" class="nav-list">Contact</a></li>
+        </ul>
+    </nav>
 
-<section id="welcome-section">
-  <h1>JASON VELARDE</h1>
-  <p class="dancingscript">web developer</p>
-</section>
-<section class="projects">
-<section id="projects">
-  <h2>My Projects</h2>
-  <div id="projects-grid">
-    <div class="imgContainer">
-        <a href="https://longislandprofessionalheadshots.com" target="_blank" class="project-tile">
-            <img src="images/lbtq.png" target="_blank" class="product-image">
-            <p>LibraryThing Quiz</p>
-        </a>
-     <div class="cntrTxt">PHP<br>+<br>JavaScript<br>+<br>MySQL</div>
-    </div>
-    <div class="imgContainer">
-        <a href="https://jvreads.com" target="_blank" class="project-tile">
-            <img src="/images/jvreads.png" target="_blank" class="product-image">
-            <p>JVReads</p>
-        </a>
-        <div class="cntrTxt">Python<br>+<br>JavaScript<br>+<br>SQLite</div>
-    </div>
-    <div class="imgContainer">
-        <a href="https://childrens.freeportlibrary.info" target="_blank" class="project-tile">
-            <img src="images/fmlcr.png" target="_blank" class="product-image">
-            <p style="font-size: 14px;">Freeport Memorial Library<br>Children's Room</p>
-        </a>
-        <div class="cntrTxt">WordPress<br>+<br>JavaScript<br>+<br>PHP</div>
-    </div>
-    <div class="imgContainer">
-        <a href="https://stemcadia.com" target="_blank" class="project-tile">
-            <img src="images/stemcadia.png" target="_blank" class="product-image">
-            <p>STEMcadia</p>
-        </a>
-        <div class="cntrTxt">WordPress<br>+<br>JavaScript<br>+<br>PHP</div>
-    </div>
-    <div class="imgContainer">
-        <a href="https://jayvel.com" target="_blank" class="project-tile">
-            <img src="images/jayvel.png" target="_blank" class="product-image">
-            <p>JayVel Photo</p>
-        </a>
-        <div class="cntrTxt">WordPress<br>+<br>JavaScript</div>
-    </div>
+    <div id="container">
+
+        <section id="welcome-section">
+            <h1>JASON VELARDE</h1>
+            <h2 class="dancingscript appear show-on-scroll">web developer</h2>
+        </section>
+
+        <section class="section" id="projects">
+            <h2>My Projects</h2>
+
+            <div class="flex-row projects-row">
+                <div class="project">                
+                    <div class="project-img">            
+                        <a href="https://jvreads.com" target="_blank"><img src="images/jvreads.png" class="from-left show-on-scroll"></a>
+                    </div>
+                    <div class="project-des">
+                        <h3 class="from-right show-on-scroll">JV Reads</h3>
+                        <p class="appear show-on-scroll">JV Reads is a readability app. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>                
+            </div>
+
+            <div class="flex-row projects-row">
+                <div class="project">                
+                    <div class="project-img">            
+                        <a href="https://stemcadia.com" target="_blank"><img src="images/stemcadia.png" class="from-left show-on-scroll"></a>
+                    </div>
+                    <div class="project-des">
+                        <h3 class="from-right show-on-scroll">STEMcadia</h3>
+                        <p class="appear show-on-scroll">STEMcadia is a wordpress blog about STEM. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>                
+            </div>
+            
+            <div class="flex-row projects-row">
+                <div class="project">                
+                    <div class="project-img">            
+                        <a href="https://jayvel.com" target="_blank"><img src="images/jayvel.png" class="from-left show-on-scroll"></a>
+                    </div>
+                    <div class="project-des">
+                        <h3 class="from-right show-on-scroll">JayVel Photography</h3>
+                        <p class="appear show-on-scroll">JayVel.com is my photography portfolio. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>                
+            </div>
+            
+            <div class="flex-row projects-row">
+                <div class="project">                
+                    <div class="project-img">            
+                        <img src="images/lbtq.png" class="from-left show-on-scroll">
+                    </div>
+                    <div class="project-des">
+                        <h3 class="from-right show-on-scroll">LibraryThing Quiz</h3>
+                        <p class="appear show-on-scroll">LibraryThing Quiz is a PHP site. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>                
+            </div>
+
+        </section>
+
+        <section class="section" id="me">
+            <h2 class="from-right show-on-scroll">About Me</h2>
+            <div class="flex-row">            
+                <div class="from-right show-on-scroll" id="mepic"></div>
+                <p class="appear show-on-scroll">Hi, I'm Jay. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </section>
     
-  </div>
-</section>
-</section>
-  
-<section id="contact-section">
-  <p>jayvelphoto@gmail.com</p>
-  <a href="https://github.com/got0values" id="profile-link" target="_blank">GitHub</a>
-</section>
-  
-</div>
+        <section class="section" id="contact">
+            <h2>Contact</h2>
+            <a href="https://github.com/got0values" id="profile-link" target="_blank">GitHub</a>
+            <div id='bottom-joke'></div>
+        </section>
+
+        <!-- <section>
+            <div id='bottom-joke'></div>
+        </section> -->
+    
+    </div>
+<script src='jv.js'></script>
+</body>
 </html>
+
+
+
+<!-- https://cssanimation.rocks/scroll-animations/ -->
