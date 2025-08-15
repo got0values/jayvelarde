d72b235b359261ad2ca4b3701b86ca6f94f6ea06
Jason Velarde <gotnovalues@gmail.com>
2025-08-14 22:31:36 -0400

    projects update 1

--- index.php
+++ index.php
@@ -103,17 +103,13 @@
                         <a href="https://garagesalesny.com/" target="_blank"><img src="/images/garagesalesnyHome.png" class="from-left show-on-scroll"></a>
                     </div>
                     <div class="project-des">
-                        <a href="https://garagesalesny.com/" target="_blank">
-                            <h3 class="from-right show-on-scroll">GarageSalesNY 
-                            <i class="fa-brands fa-node"></i>
+                        <a href="https://wordpress.org/plugins/terms-gate/" target="_blank">
+                            <h3 class="from-right show-on-scroll">Terms Gate 
+                            <i class="fa-brands fa-php"></i>
                             </h3>
-                        </a>
-                        <div class="mb-3">
-                            <p>u/n: <i>DEMO</i></p>
-                            <p>p/w: <i>DEMOPW</i></p>
-                        </div>                        
-                        <a href="https://github.com/got0values/garagesalesny" target="_blank"><button>View Code</button></a>
-                        <p class="appear show-on-scroll ln-ht">A web app for users to find garage sales in New York.<br><br><b>Technologies used:</b><br><i>HTML, CSS, Bootstrap, NodeJS, MongoDB, Google Maps API, Google Geocoding API</i></p>
+                        </a>                       
+                        <a href="https://github.com/got0values/terms-gate" target="_blank"><button>View Code</button></a>
+                        <p class="appear show-on-scroll ln-ht">A Wordpress plugin that lets users require visitors to agree to terms, privacy policy, or any custom agreement before they can view specific posts or pages.<br><br><b>Technologies used:</b><br><i>PHP, Wordpress</i></p>
                     </div>
                 </div>                
 
@@ -151,55 +147,6 @@
                     </div>
                 </div>   
                 
-            </div>
-
-            <div class="projects-row">
-                <div class="project col-lg">                
-                    <div class="project-img">            
-                        <a href="https://jayvel-photo-development-store.myshopify.com/" target="_blank"><img src="/images/jayvel-photo-development-store.myshopify.com_.png" class="from-left show-on-scroll"></a>
-                    </div>
-                    <div class="project-des">
-                        <a href="https://jayvel-photo-development-store.myshopify.com/" target="_blank">
-                          <h3 class="from-right show-on-scroll">
-                            JVPhoto Shopify Theme <i class="fa-brands fa-shopify"></i>
-                          </h3>
-                        </a>
-                        <div class="mb-3">
-                            <p>p/w: <i>DEMOPW</i></p>
-                        </div>
-                        <a href="https://github.com/got0values/jvphoto" target="_blank"><button>View Code</button></a>
-                        <p class="appear show-on-scroll ln-ht">A photography-themed Shopify theme.<br><br><b>Technologies used:</b><br><i>HTML, CSS, Bootstrap, Liquid, JavaScript</i></p>
-                    </div>
-                </div> 
-
-                <div class="project col-lg">                
-                    <div class="project-img">            
-                        <a href="https://github.com/got0values/knaptime" target="_blank"><img src="/images/knaptimescreenshot.png" class="from-left show-on-scroll"></a>
-                    </div>
-                    <div class="project-des">
-                        <a href="https://github.com/got0values/knaptime" target="_blank">
-                            <h3 class="from-right show-on-scroll">
-                              Knaptime <i class="fa-brands fa-magento"></i>
-                            </h3>
-                        </a>                    
-                        <a href="https://github.com/got0values/knaptime" target="_blank"><button>View Code</button></a>
-                        <p class="appear show-on-scroll ln-ht">A demo eCommerce site made with Magento.<br><br><b>Technologies used:</b><br><i>HTML, CSS, PHP, Javascript, Magento, Linux, GCP Compute Engine</i></p>
-                    </div>
-                </div>
-
-                <div class="project col-lg">                
-                    <div class="project-img">            
-                        <a href="https://stemcadia.com/" target="_blank"><img src="/images/stemcadia.png" class="from-left show-on-scroll"></a>
-                    </div>
-                    <div class="project-des">
-                        <a href="https://stemcadia.com/" target="_blank">
-                            <h3 class="from-right show-on-scroll">STEMcadia 
-                            <i class="fa-brands fa-wordpress"></i>
-                            </h3>
-                        </a>
-                        <p class="appear show-on-scroll ln-ht">An informational website on all things Science, Technology, Engineering, and Math.<br><br>Technologies used:<br><i>WordPress, HTML, CSS, PHP, JavaScript</i></p>
-                    </div>
-                </div>         
             </div>
         </section>
 
